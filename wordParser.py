13d986188ac83f64df65d8b8022b65ce52195d9a
wScottSh <7772230+wScottSh@users.noreply.github.com>
2018-02-13 17:05:04 -0600

    adds parsing functions

--- wordParser.py
+++ wordParser.py
@@ -1,10 +1,41 @@
-import csv
+import citylist
 
-def writeDictionary():
-    for v in csv:
-        toDict(v)
+cityList = [ 'Neo',
+  'Max Tessenholtz',
+  'Marcy Lina',
+  'Christopher Hanten',
+  'Jorge Rodriguez',
+  'Aleena Madni',
+  'Tess Craig',
+  'Jake Chavez',
+  'Scott Sheppard',
+  'Lillian Chernin' ]
 
-def toDict(word):
-    for i in range(i+2, len(word-2)):
-        chunk = word[i-2:i]
-        dictionary["chunk"] += 1
+def writeDictionary(list):
+    chunky = {}
+    firstLetters = {}
+    for word in list:
+        chunkFreq(word, chunky)
+        firstLetterFreq(word, firstLetters)
+    print(chunky)
+    print(firstLetters)
+
+def chunkFreq(word, chunky):
+    for i in range(3, len(word)+1):
+        chunk = word[i-3:i].lower()
+        if chunk in chunky:
+            chunky[chunk] += 1
+        else:
+            chunky[chunk] = 1
+
+def firstLetterFreq(word, firstLetters):
+    firstLetter = word[0].lower()
+    if firstLetter in firstLetters:
+        firstLetters[firstLetter] += 1
+    else:
+        firstLetters[firstLetter] = 1
+
+def nameLengthFreq:
+    pass # add functionality here
+
+writeDictionary(cityList)
