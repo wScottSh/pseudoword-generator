f4e361afc9995dab54a2fb67729090cafd155b7b
wScottSh <7772230+wScottSh@users.noreply.github.com>
2018-02-16 18:14:31 -0600

    WIP got me some random words popping up, but its borked rn

--- generator.py
+++ generator.py
@@ -5,30 +5,55 @@ from harddata.chunks import ternChunk
 import random
 import re
 
-def wRandFrmDct(dct): # define function that takes a dictionary
-    rand_val = random.randint(1, sum(dct.values()))   # pulls in a random number from the random module and assigns it to rand_val
-    total = 0                    # creates a running total and defults it to 0
-    for k, v in dct.items():     # pulls the key and value tuple from the dictionary
-        total += v               # adds the value of each key to the total variable (this is creating the weighted-ness)
-        if rand_val <= total:    # if the random input is <= to total (this is the issue, need to normalize first)
+def wRandFrmDct(dct):
+    rand_val = random.randint(1, sum(dct.values()))
+    total = 0
+    for k, v in dct.items():
+        total += v
+        if rand_val <= total:
             return k
     assert False, 'unreachable'
 
 def nameGenerator():
     length = wRandFrmDct(nameLength)
+    print(length)
     firstLetter = wRandFrmDct(startingLetter)
     pseudoCityName = ''
+    paredChunk = {}
 
-    #TODO:look at all the keys in dct that start with the firstLetter var and append that triple to the pseudoCityName
+    # Assign
+    for k,v in ternChunk.items():
+        if re.match(r"^{}".format(firstLetter), k):
+            if k in paredChunk:
+                paredChunk[k] += 1
+            else:
+                paredChunk[k] = 1
+            paredChunk[k] += v
+    # print(paredChunk)
+    pseudoCityName += wRandFrmDct(paredChunk)
+    paredChunk = {}
+    print(pseudoCityName)
 
-    for i in range(1, length-2): # run this code up until it fills the length of the word
+    for i in range(2,int(length)-1): # run this code up until it fills the length of the word
+        for k,v in ternChunk.items():
+            if re.match(r"{}$".format(pseudoCityName[-2:]), k):
+                if k in paredChunk:
+                    paredChunk[k] += 1
+                else:
+                    paredChunk[k] = 1
+                paredChunk[k] += v
+        pseudoCityName += wRandFrmDct(paredChunk)
+        print(pseudoCityName)
+
         #TODO:look at final two letters in pseudoCityName and compare to first two letters of chunk
         #TODO:weighted-randomly select from that pared list and append the final character to the pseudoCityName variable
 
     #TODO:once two chars from end of word, append a weighted triple from wordEnds dictionary based on one overlapping letter
-        return pseudoCityName
+        # return pseudoCityName
 
-for k,v in ternChunk.items():
-    var = la
-    if re.match(r"^{}".format(var), k):
-        print(k,v)
+# for k,v in ternChunk.items():
+#     var = 'ce'
+#     if re.match(r"^{}".format(var), k):
+#         print(k,v)
+
+nameGenerator()
